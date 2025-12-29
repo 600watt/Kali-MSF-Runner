89e356af6f7b4fae72627eb737c8dd1388dc9fbc
Jave <muhammedsaitaltug@gmail.com>
2025-12-29 11:15:36 +0300

    Giris ekrani guncellendi: Sadece 600WATT

--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-echo -e "\033[1;32m[*] Kali Linux Bağlantısı Kuruluyor...\033[0m"
-echo -e "\033[1;31m[*] Metasploit Framework Başlatılıyor! \033[0m"
-
+clear
+echo -e "\033[1;31m"
+echo "  __   ___   ___ __      __  _  _______ _______ "
+echo " / /  / _ \ / _ \\ \    / / / |/_/  _// ___/ /"
+echo "/ _ \/ // // // /\ \/\/ / / __// // / /_   "
+echo "\___/\___/ \___/  \_{_}/ /_/  /___/ \__/   "
+echo -e "\033[1;32m"
+echo "           [*] 600WATT [*]           "
+echo -e "\033[1;34m"
+echo "[*] Kali NetHunter Bağlantısı Kuruluyor..."
+echo "[*] Metasploit Framework Başlatılıyor..."
+echo -e "\033[0m"
 sleep 2
 
-
 nh msfconsole
-
